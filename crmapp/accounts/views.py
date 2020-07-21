0bf861ace87c7f46d82cc084405a7f737bb476cb
alikurtulush92 <kopanarya92@gmail.com>
2020-07-21 09:45:33 +0100

    added order form crud

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 # Create your views here.
 def home(request):
@@ -25,5 +26,33 @@ def products(request):
     return render(request, 'accounts/products.html', {'products':products})
 
 def createOrder(request):
-    context = {}
+    form = OrderForm()
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
     return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance=order)
+        form.save()
+        return redirect('/')
+    context = {'form':form}
+    return render(request,'accounts/order_form.html',context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+
+    context = {'item':order}
+    return render(request,'accounts/delete_order.html', context)
+
+
