ea896c45b4a70e5b9385fca0dccbdb0fdd4f0dac
alikurtulush92 <kopanarya92@gmail.com>
2020-07-28 21:06:51 +0100

    added some decorators for authentication

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -8,42 +8,42 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user, allowed_users, admin_only
+from django.contrib.auth.models import Group
 
+
+@unauthenticated_user
 def registerPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if request.method == "POST":
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request,'Account was created for ' + user )
-                return redirect('login')
-        context = {'form':form}
-        return render(request, 'accounts/register.html',context)
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username= form.cleaned_data.get('username')
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+           
+            messages.success(request,'Account was created for ' + username )
+            return redirect('login')
+    context = {'form':form}
+    return render(request, 'accounts/register.html',context)
         
-   
 
-
+@unauthenticated_user
 def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         user = authenticate(request, username=username, password = password)
-
+        
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
             messages.info(request, 'Username Or password incorrect')
-            
-
     context = {}
     return render(request, 'accounts/login.html',context)
-
 
 def logoutUser(request):
     logout(request)
@@ -51,6 +51,7 @@ def logoutUser(request):
 
 # Create your views here.
 @login_required(login_url="login")
+@admin_only
 def home(request):
     customers = Customer.objects.all()
     orders = Order.objects.all()
@@ -62,6 +63,7 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 @login_required(login_url="login")
+@allowed_users(allowed_roles=['admin'])
 def customer(request,pk):
     customer = Customer.objects.get(id=pk) 
     orders = customer.order_set.all()
@@ -72,11 +74,13 @@ def customer(request,pk):
     return render(request, 'accounts/customer.html', context)
 
 @login_required(login_url="login")
+@allowed_users(allowed_roles=['admin'])
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products':products})
 
 @login_required(login_url="login")
+@allowed_users(allowed_roles=['admin'])
 def createOrder(request,pk):
     OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'), extra=10 )
     customer = Customer.objects.get(id=pk)
@@ -93,6 +97,7 @@ def createOrder(request,pk):
     return render(request, 'accounts/order_form.html', context)
 
 @login_required(login_url="login")
+@allowed_users(allowed_roles=['admin'])
 def updateOrder(request,pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -104,13 +109,15 @@ def updateOrder(request,pk):
     return render(request,'accounts/order_form.html',context)
 
 @login_required(login_url="login")
+@allowed_users(allowed_roles=['admin'])
 def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
     if request.method == "POST":
         order.delete()
         return redirect('/')
-
     context = {'item':order}
     return render(request,'accounts/delete_order.html', context)
 
-
+def userPage(request):
+    context = {}
+    return render(request,'accounts/user.html', context)
