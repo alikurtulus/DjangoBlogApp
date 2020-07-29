a60002f6f11100715cc162c6e537a37bf4a11350
alikurtulush92 <kopanarya92@gmail.com>
2020-07-29 10:02:08 +0100

    added user and customer relationship

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -22,13 +22,15 @@ def registerPage(request):
             username= form.cleaned_data.get('username')
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+            )
            
             messages.success(request,'Account was created for ' + username )
             return redirect('login')
     context = {'form':form}
     return render(request, 'accounts/register.html',context)
         
-
 @unauthenticated_user
 def loginPage(request):
     if request.method == 'POST':
@@ -118,6 +120,13 @@ def deleteOrder(request, pk):
     context = {'item':order}
     return render(request,'accounts/delete_order.html', context)
 
+@login_required(login_url="login")
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    print('orders:', orders)
+    context = {'orders': orders,'total_orders':total_orders,'delivered':delivered, 'pending': pending}
     return render(request,'accounts/user.html', context)
